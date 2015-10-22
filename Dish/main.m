a8fa583838787c914cf7474690d097ca2f2c8fc8
Jedd Goble <jeddgoble@gmail.com>
2015-10-22 15:41:13 -0700

    Initial Commit

---
+++ Dish/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dish
+//
+//  Created by Jedd Goble on 10/22/15.
+//  Copyright © 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
