8a378a8ce2317939d197d49006be7522db679941
Jedd Goble <Jedd@Jedds-MacBook-Pro.local>
2015-10-28 13:23:10 -0700

    working on photo subclass. basics are now working

--- Dish/Photo.h
+++ Dish/Photo.h
@@ -8,6 +8,19 @@
 
 #import <Parse/Parse.h>
 
-@interface Photo : PFObject
+
+@interface Photo : PFObject <PFSubclassing>
+
+@property (strong, nonatomic) NSString *objectID;
+@property (strong, nonatomic) NSString *photoDesc_string;
+@property (strong, nonatomic) NSString *photoTitle_string;
+@property (strong, nonatomic) NSData *photo_data;
+@property (strong, nonatomic) UIImage *image;
+@property (strong, nonatomic) NSDate *createdAt;
+@property (strong, nonatomic) NSDate *updatedAt;
+
+
++ (NSString *) parseClassName;
+
 
 @end
--- Dish/Photo.m
+++ Dish/Photo.m
@@ -7,9 +7,32 @@
 //
 
 #import "Photo.h"
+#import <Parse/PFObject+Subclass.h>
 
 @implementation Photo
 
+@dynamic objectID;
+@dynamic photoDesc_string;
+@dynamic photoTitle_string;
+@dynamic photo_data;
+@dynamic image;
+@dynamic createdAt;
+@dynamic updatedAt;
 
+- (void)setPhoto_data:(NSData *)photo_data {
+    
+    self.photo_data = photo_data;
+    self.image = [UIImage imageWithData:photo_data];
+    
+    
+}
+
++ (void) load {
+    [self registerSubclass];
+}
+
++ (NSString *) parseClassName {
+    return @"Photo";
+}
 
 @end
