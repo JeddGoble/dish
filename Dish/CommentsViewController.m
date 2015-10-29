ab0dbe773a58290db2ad9b328991eda442e19292
Jedd Goble <jeddgoble@gmail.com>
2015-10-29 13:21:13 -0700

    committing before merge

--- Dish/CommentsViewController.m
+++ Dish/CommentsViewController.m
@@ -19,6 +19,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.tableView.estimatedRowHeight = 60.0;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    
     
 }
 
